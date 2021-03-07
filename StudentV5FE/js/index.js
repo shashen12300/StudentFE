0efa16c9d6a92083e5c154d0032cf529a5a290f4
songchongchong <719121216@qq.com>
2021-03-07 10:24:13 +0800

    更新框架代码

--- StudentV5FE/js/index.js
+++ StudentV5FE/js/index.js
@@ -2,4 +2,49 @@ const app = new Vue({
     el: '#app',
     data: {
         msg: 'Hello Vue!',
+        students: [
+            {
+                sno: 95001, name: '刘建辉', gender: '男', birthday: '1991年7月23日',
+                email: 'liujianhui@qq.com', address: '北京市西城区展览路'
+            },
+            {
+                sno: 95002, name: '陈燕', gender: '女', birthday: '1991年6月23日',
+                email: 'chenyan@qq.com', address: '北京市西城车公庄大街'
+            },
+            {
+                sno: 95003, name: '马建', gender: '男', birthday: '1991年7月26日',
+                email: 'majian@qq.com', address: '北京市石景山区古城北路'
+            },
+            {
+                sno: 95001, name: '刘建辉', gender: '男', birthday: '1991年7月23日',
+                email: 'liujianhui@qq.com', address: '北京市西城区展览路'
+            },
+            {
+                sno: 95002, name: '陈燕', gender: '女', birthday: '1991年6月23日',
+                email: 'chenyan@qq.com', address: '北京市西城车公庄大街'
+            },
+            {
+                sno: 95003, name: '马建', gender: '男', birthday: '1991年7月26日',
+                email: 'majian@qq.com', address: '北京市石景山区古城北路'
+            },
+            {
+                sno: 95001, name: '刘建辉', gender: '男', birthday: '1991年7月23日',
+                email: 'liujianhui@qq.com', address: '北京市西城区展览路'
+            },
+            {
+                sno: 95002, name: '陈燕', gender: '女', birthday: '1991年6月23日',
+                email: 'chenyan@qq.com', address: '北京市西城车公庄大街'
+            },
+            {
+                sno: 95003, name: '马建', gender: '男', birthday: '1991年7月26日',
+                email: 'majian@qq.com', address: '北京市石景山区古城北路'
+            },
+            {
+                sno: 95001, name: '刘建辉', gender: '男', birthday: '1991年7月23日',
+                email: 'liujianhui@qq.com', address: '北京市西城区展览路'
+            },
+        ],
+        total: 100, //数据总行数
+        currentpage:1, //当前所在页
+        pagesize:10, //每页显示行数
     }});
